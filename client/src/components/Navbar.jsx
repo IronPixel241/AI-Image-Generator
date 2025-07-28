35d5c7bb08fc52aa2ba46020960343315ba1c9fb
Sounak Nandi <sounaknandi24@gmail.com>
2025-07-28 19:51:47 +0530

    Simple logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import logo from '../assets/logo.svg'
+// import logo from '../assets/logo.svg'
+import {assets} from '../assets/assets'
 
 const Navbar = () => {
   return (
     <div>
-        <img src={logo} alt="Logo" className='w-28 sm:w-32 lg:w-40' />
+        <img src={assets.logo} alt="Logo" className='w-28 sm:w-32 lg:w-40' />
     </div>
   )
 }
