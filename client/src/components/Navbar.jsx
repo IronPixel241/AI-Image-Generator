f59e1d26123a49629d556f1be79b7ee3fa36613b
Sounak Nandi <sounaknandi24@gmail.com>
2025-07-30 22:20:23 +0530

    Added AppContextProvider

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-// import logo from '../assets/logo.svg'
 import {assets} from '../assets/assets'
-import {Link} from 'react-router-dom'
-import {useState} from 'react'
+import {Link, useNavigate} from 'react-router-dom'
 
 const Navbar = () => {
 
 
-  const [user, setUser] = useState(null);
+
+  const navigate = useNavigate();
 
   return (
     <div className='flex justify-between items-center py-4'>
@@ -20,12 +19,29 @@ const Navbar = () => {
       <div>
 
         {user ? 
-        <div>
-          
+        <div className='flex items-center gap-6'>
+
+          <button className=' cursor-pointer flex items-center gap-2 bg-blue-100 px-6 py-3 rounded-full hover:scale-105 transition-all duration-700'>
+            <img src={assets.credit_star} alt="" className='w-6 h-6' />
+            <p className='text-xs font-medium text-gray-600'>Credits Left : 50</p>
+          </button>
+
+          <p className='pl-2 text-gray-600 max-sm:hidden'>Hi, Sounak</p>
+
+          <div className='relative group'>
+            <img src={assets.profile_icon} alt="" className='w-10 drop-shadow' />
+            <div className='absolute hidden group-hover:block top-0 
+            right-0 z-10 text-black rounded pt-12'>
+              <ul className='list-none m-0 p-2 bg-white rounded-md border text-sm'>
+                <li className='py-1 px-2 cursor-pointer pr-10'>Logout</li>
+              </ul>
+            </div>
+          </div>
+
         </div> 
         : 
         <div className='flex items-center gap-4'>
-          <p className='cursor-pointer'>Pricing</p>
+          <p onClick={() => navigate('/buy')} className='cursor-pointer'>Pricing</p>
           <button className='bg-black text-white px-15 py-2 sm:py-3 text-sm rounded-full'>Login</button>
         </div>
         }
